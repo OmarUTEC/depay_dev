d05444c3c2a04196caf80706c86e511f6fe28c05
OmarUTEC <javier.chavarria@utec.edu.pe>
2025-02-20 18:48:33 -0500

    All changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/depay_dev/',
+  base: './',
   plugins: [react()],
   css: {
     preprocessorOptions: {
